c71cce656806aac73a6bcd9c398337f7cbba9040
Romain <romain@rherault.fr>
2022-11-06 19:25:44 +0100

    init: adding base files

---
+++ wp-turbo.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Wp Turbo
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     Integrate Turbo with WordPress.
+ * Author:          Romain Herault
+ * Author URI:      https://rherault.fr
+ * Text Domain:     wp-turbo
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wp_Turbo
+ */
+
+// Your code starts here.
